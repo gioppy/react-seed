563e0a5c432d4008c847f59c1f2e06ad79c479e6
gioppy <tech@giovannibuffa.it>
2016-09-30 09:23:10 +0200

    Initial development release

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>React Seed</h1>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {},
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
